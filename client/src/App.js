1aeab8a0f5398c802d7f732984e6ed0e31ef1d59
Kurtis Houser <houserkurtis@gmail.com>
2018-02-08 12:20:19 -0800

    Add SF Leaflet map using React-Leaflet component

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
+import LeafletMap from './components/LeafletMap';
 import logo from './logo.svg';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h1 className="App-title">Resiliency Map</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <LeafletMap />
       </div>
     );
   }
